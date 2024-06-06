fec9846cb3106a4c6262b029c2e40216977082bf
bbaiserkeev <bbaiserkeev@s-pro.io>
2024-06-07 00:25:19 +0600

    message

---
+++ my_function/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import azure.functions as func
+from fastapi import FastAPI, Request
+from fastapi.routing import APIRoute
+from pydantic import BaseModel, Field
+from typing import Callable
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str = Field(..., example="Item name")
+    description: str = Field(..., example="Item description")
+
+
+@app.get("/items/")
+async def read_item(name: str):
+    return {"name": name}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+# Azure Function HttpTrigger
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    def fastapi_app(req: Request) -> Callable:
+        return app
+
+    return func.AsgiMiddleware(fastapi_app).handle(req, context)
