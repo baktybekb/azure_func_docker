4a71172edddb66655982fa28aa5751f8e4b43c50
bbaiserkeev <bbaiserkeev@s-pro.io>
2024-06-07 17:34:25 +0600

    docker deploy

--- my_function/__init__.py
+++ my_function/__init__.py
@@ -1,8 +1,10 @@
 import logging
 import azure.functions as func
 from my_function.settings import settings
+from pydantic import BaseModel
 
 
 def main(mytimer: func.TimerRequest) -> None:
     logging.info(f"API Key: {settings.api_key}")
     logging.info('Buuhaaaaaaaaaaaaaaaaaaaaaaa')
+    logging.info('Docker image deployment version')
