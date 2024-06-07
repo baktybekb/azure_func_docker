4b4b7e5deb2b15870168d43f44a7c8488efb5ee6
bbaiserkeev <bbaiserkeev@s-pro.io>
2024-06-07 16:20:58 +0600

    message

--- my_function/__init__.py
+++ my_function/__init__.py
@@ -1,22 +1,8 @@
-import datetime
 import logging
-import os
 import azure.functions as func
-from pydantic import BaseModel
-from fastapi import FastAPI
-
-
-class Item(BaseModel):
-    name: str
-    description: str
+from my_function.settings import settings
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    item = Item(name="Example", description="This is a test item")
-    logging.info(f"Python timer trigger function ran at {utc_timestamp}")
-    logging.info(f"Item: {item.json()}")
-
-    logging.info(os.getenv('BAHA'))
+    logging.info(f"API Key: {settings.api_key}")
+    logging.info('Buuhaaaaaaaaaaaaaaaaaaaaaaa')
