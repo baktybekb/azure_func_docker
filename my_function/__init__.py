ce4d9a4a280fafc7af62ab5c71f5cb46fa3fcfa3
bbaiserkeev <bbaiserkeev@s-pro.io>
2024-06-09 23:21:49 +0600

    prev deploy

--- my_function/__init__.py
+++ my_function/__init__.py
@@ -7,4 +7,5 @@ from pydantic import BaseModel
 def main(mytimer: func.TimerRequest) -> None:
     logging.info(f"API Key: {settings.api_key}")
     logging.info('Buuhaaaaaaaaaaaaaaaaaaaaaaa')
-    logging.info('Docker image deployment version')
+    logging.info('Docker image deployment version ------------ ======')
+
