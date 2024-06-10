3e7f0a5b5e1669ffe257e7123de44cafd48345e7
bbaiserkeev <bbaiserkeev@s-pro.io>
2024-06-10 16:50:56 +0600

    prev deploy

--- azure_function/__init__.py
+++ azure_function/__init__.py
@@ -7,12 +7,12 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.info('Bahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
     logging.info('=' * 20)
 
-    container = Container()
-    with container.ldap_connection() as conn:
-        ldap_service = container.ldap_service(connection=conn)
-        app = container.ldap_application(ldap_service=ldap_service)
-        config = container.config()
-        app.run(config.ldap_search_base, config.group_patterns_list)
+    # container = Container()
+    # with container.ldap_connection() as conn:
+    #     ldap_service = container.ldap_service(connection=conn)
+    #     app = container.ldap_application(ldap_service=ldap_service)
+    #     config = container.config()
+    #     app.run(config.ldap_search_base, config.group_patterns_list)
 
 # if __name__ == '__main__':
 #     container = Container()
